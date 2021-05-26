1c139113c0275ffc260bdcd86946c7bee47f8be8
halilerhan.orun <erhan0494@gmail.com>
2021-05-26 20:45:45 +0300

    No change

---
+++ calisma.py
@@ -0,0 +1,13 @@
+def decorate_message(fun):
+    def addWelcome(site_name):
+        return "Welcome to " + fun(site_name)
+
+    return addWelcome
+
+
+@decorate_message
+def site(site_name):
+    return site_name
+
+
+print(site("GeeksforGeeks"))
---
+++ time_series.py
@@ -0,0 +1,51 @@
+import plotly.graph_objects as go
+from tkinterhtml import HtmlFrame
+import pandas as pd
+
+# Load data
+df = pd.read_csv("data.csv")
+df.columns = [col.replace("AAPL.", "") for col in df.columns]
+
+# Create figure
+fig = go.Figure()
+
+fig.add_trace(
+    go.Scatter(x=list(df.Date), y=list(df.High)))
+
+# Set title
+fig.update_layout(
+    title_text="Time series with range slider and selectors"
+)
+
+# Add range slider
+fig.update_layout(
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label="1m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=6,
+                     label="6m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="YTD",
+                     step="year",
+                     stepmode="todate"),
+                dict(count=1,
+                     label="1y",
+                     step="year",
+                     stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
+        rangeslider=dict(
+            visible=True
+        ),
+        type="date"
+    )
+)
+
+fig.show()
