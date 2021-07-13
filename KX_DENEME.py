f5a4741956202501741f85776d50840dafa7de00
halilerhan.orun <erhan0494@gmail.com>
2021-07-13 15:29:30 +0300

    13.07.2021 15:29

---
+++ KX_DENEME.py
@@ -0,0 +1,10 @@
+iletken_tipi = "6_PE", "6_CPE", "5_PE", "5_CPE", "4.5", "4"
+malzeme_tipi = "AL", "CU"
+kesit = 55
+uretilen_iletken = 6
+boy = 2962
+
+if iletken_tipi == "6_PE" and malzeme_tipi == "AL" and kesit == 55:
+    print(uretilen_iletken * boy)
+else:
+    print("degil")
